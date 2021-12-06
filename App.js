ee75c078016d85058448414294cec406c48bb9ea
thisisjash <thisisjash619@gmail.com>
2021-12-07 00:28:57 +0530

    Minor Changes before Publishing on Expo

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x = 8;
   return (
     <View style={styles.container}>
       <Text>Love You, Leela!</Text>
